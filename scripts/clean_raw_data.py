6b6140452810ec51f9b37f65e80fc03c11259587
David Solow <davesolow@gmail.com>
2024-07-29 20:09:44 -0400

    Eval embeddings model

--- scripts/clean_raw_data.py
+++ scripts/clean_raw_data.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 
-train = pd.read_csv("data/medal_train.csv")
-validation = pd.read_csv("data/medal_val.csv")
-test = pd.read_csv("data/medal_test.csv")
+train = pd.read_csv("data/medal_smaller_train.csv")
+validation = pd.read_csv("data/medal_smaller_validation.csv")
+test = pd.read_csv("data/medal_smaller_test.csv")
 print("Train dataset length:", len(train))
 print("Validation dataset length:", len(validation))
 print("Test dataset length:", len(test))
@@ -18,28 +18,28 @@ def process_label(label):
     return eval(label)[0]
 
 # Reading and processing training data
-train_df = pd.read_csv('data/medal_train.csv')
-train_df['location'] = train_df['location'].map(process_location)
-train_df['label'] = train_df['label'].map(process_label)
-train_df['abbreviation'] = train_df.apply(lambda row: row['text'].split()[row['location']], axis=1)
+train_df = pd.read_csv('data/medal_smaller_train.csv')
+#train_df['location'] = train_df['location'].map(process_location)
+#train_df['label'] = train_df['label'].map(process_label)
+#train_df['abbreviation'] = train_df.apply(lambda row: row['text'].split()[row['location']], axis=1)
 train_df = train_df[train_df['location'] <= max_location]
 
 # Reading and processing validation data
-val_df = pd.read_csv('data/medal_val.csv')
-val_df['location'] = val_df['location'].map(process_location)
-val_df['label'] = val_df['label'].map(process_label)
-val_df['abbreviation'] = val_df.apply(lambda row: row['text'].split()[row['location']], axis=1)
+val_df = pd.read_csv('data/medal_smaller_validation.csv')
+# val_df['location'] = val_df['location'].map(process_location)
+# val_df['label'] = val_df['label'].map(process_label)
+# val_df['abbreviation'] = val_df.apply(lambda row: row['text'].split()[row['location']], axis=1)
 val_df = val_df[val_df['location'] <= max_location]
 
 # Reading and processing test data
-test_df = pd.read_csv('data/medal_test.csv')
-test_df['location'] = test_df['location'].map(process_location)
-test_df['label'] = test_df['label'].map(process_label)
-test_df['abbreviation'] = test_df.apply(lambda row: row['text'].split()[row['location']], axis=1)
+test_df = pd.read_csv('data/medal_smaller_test.csv')
+# test_df['location'] = test_df['location'].map(process_location)
+# test_df['label'] = test_df['label'].map(process_label)
+# test_df['abbreviation'] = test_df.apply(lambda row: row['text'].split()[row['location']], axis=1)
 test_df = test_df[test_df['location'] <= max_location]
 
-train_df.to_parquet("data/train_df.parquet")
-val_df.to_parquet("data/val_df.parquet")
-test_df.to_parquet("data/test_df.parquet")
+train_df.to_parquet("data/medal_smaller_train_df.parquet")
+val_df.to_parquet("data/medal_smaller_val_df.parquet")
+test_df.to_parquet("data/medal_smaller_test_df.parquet")
 
 print("Data cleaned and DFs saved in data folder.")
